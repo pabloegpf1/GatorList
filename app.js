be972e6e8e628d32303d7d52efd6c8562550f902
stephaniesantana <stephycsantana@gmail.com>
2018-09-19 17:57:25 -0700

    Added in routes and ejs files for all team

--- app.js
+++ app.js
@@ -20,6 +20,35 @@ app.get("/aboutStephanie.html", (req, res)=> {
 	res.render("aboutStephanie")
 })
 
+app.get("/aboutPablo.html", (req, res)=> {
+
+	res.render("aboutPablo")
+})
+
+app.get("/aboutJohnny.html", (req, res)=> {
+
+	res.render("aboutJohnny")
+})
+
+app.get("/aboutSyed.html", (req, res)=> {
+
+	res.render("aboutSyed")
+})
+
+app.get("/aboutJack.html", (req, res)=> {
+
+	res.render("aboutJack")
+})
+
+app.get("/aboutMarlo.html", (req, res)=> {
+
+	res.render("aboutMarlo")
+})
+
+app.get("/aboutHarry.html", (req, res)=> {
+
+	res.render("aboutHarry")
+})
 app.listen(process.env.PORT || 3000, ()=> {
 
 	console.log("Server Running")
