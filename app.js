3832814d88ba491c671f9c8989dd1ad5e644a5c0
stephaniesantana <stephycsantana@gmail.com>
2018-09-19 17:50:10 -0700

    Updated missing route for /aboutStephanie

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.get("/index.html", (req, res)=> {
 	res.render("index")
 })
 
+app.get("/aboutStephanie.html", (req, res)=> {
+
+	res.render("aboutStephanie")
+})
+
 app.listen(process.env.PORT || 3000, ()=> {
 
 	console.log("Server Running")
