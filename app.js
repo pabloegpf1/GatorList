8ea5e29495a974c5075ea8fcaca5ec21007bc039
pabloegpf1 <pabloescrivagallardo@gmail.com>
2018-09-20 20:21:41 -0700

    v1.0: About page

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=> {
+
+	res.render("index")
+})
+
+app.get("/index.html", (req, res)=> {
+
+	res.render("index")
+})
+
+app.get("/aboutStephanie.html", (req, res)=> {
+
+	res.render("aboutStephanie")
+})
+
+app.get("/aboutPablo.html", (req, res)=> {
+
+	res.render("aboutPablo")
+})
+
+app.get("/aboutJohnny.html", (req, res)=> {
+
+	res.render("aboutJohnny")
+})
+
+app.get("/aboutSyed.html", (req, res)=> {
+
+	res.render("aboutSyed")
+})
+
+app.get("/aboutJack.html", (req, res)=> {
+
+	res.render("aboutJack")
+})
+
+app.get("/aboutMarlo.html", (req, res)=> {
+
+	res.render("aboutMarlo")
+})
+
+app.get("/aboutHarry.html", (req, res)=> {
+
+	res.render("aboutHarry")
+})
+
+app.listen(process.env.PORT || 3000, ()=> {
+
+	console.log("Server Running")
+})
