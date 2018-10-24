dcd67760191526edad6ebbb46f2ede491d0772fd
Greedhunter <shiyilijack@gmail.com>
2018-10-24 16:55:32 -0700

    updates on Index2

--- routes/index2.js
+++ routes/index2.js
@@ -12,40 +12,41 @@ router.get("/", (req, res, next)=> {
 router.get("/index", (req, res)=> {
    var Category = req.body.category;
    var Search = req.body.search;
-if (req.body.category == 'Category'){
-   router.get("/index", (req, res, next)=> {
+   if (req.body.category == 'Category'){
+      router.get("/index", (req, res, next)=> {
       knex('Items')
       .then(
          knex.select('picture', 'Title', 'userID').from('Items')
-         .then(function(items) {
-            res.render('users',{items: items});
+      .then(function(items) {
+         res.render('users',{items: items});
          }));
    })
-}else {
-   router.get("/categorySearch", (req, res, next)=> {
-      knex('Items')
-      .where('Category', Category)
-      .then(function(items) {
+   }else {
+      router.get("/categorySearch", (req, res, next)=> {
+         knex('Items')
+         .where('Category', Category)
+         .then(function(items) {
             res.render('items',{items: items});
          });
-   })
-}
-if(req.body.search == ""){
-   knex('Items')
-   .then(function(items) {
-      res.render('items',{items: items});
+      })
    }
-}else{
-   router.post("/item-search", (req, res, next)=> {
-      console.log("Searching for: "+req.body.search);
+   if(req.body.search == ""){
       knex('Items')
-      .where('ItemName', 'like,' '%'Search'%')
-      .orWhere('ItemDescription', 'like,' '%'+Search+'%')
       .then(function(items) {
          res.render('items',{items: items});
-      });
-   })
-}
+      })
+   }else{
+      router.post("/item-search", (req, res, next)=> {
+         console.log("Searching for: " + req.body.search);
+         knex('Items')
+         .where('ItemName', 'like', '%'+Search+'%')
+         .orWhere('ItemDescription', 'like', '%'+Search+'%')
+         .then(function(items) {
+            res.render('items',{items: items});
+         });
+      })
+   }
+});
 
 
 
