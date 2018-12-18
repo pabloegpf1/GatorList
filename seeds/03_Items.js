ee0744135b6496f1ac0b31b3a2553e8c3d7b3a70
Stephanie Santana <31974473+StephanieSantana@users.noreply.github.com>
2018-12-17 18:31:03 -0800

    Removed Lorem Ipsum text descriptions

--- seeds/03_Items.js
+++ seeds/03_Items.js
@@ -9,17 +9,17 @@ exports.seed = function(knex, Promise) {
       Title: 'Databases book',
       UserID: 500,
       Category: 'Books',
-      Price: 12,
-      Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam aliquam sem et tortor consequat. Gravida arcu ac tortor dignissim. A pellentesque sit amet porttitor eget dolor morbi non. Nam aliquam sem et tortor consequat id porta.',
+      Price: 50,
+      Description: 'Only used for 1 semester. It is required for CSC 675. Okay condition, there is highlighted text in the book and a few pages have small corner tears.',
       Approved: true
     },
     {
       ID: 2000,
       Title: 'Batteries',
       UserID: 300,
-      Price: 3,
+      Price: 6,
       Category: 'Electronics',
-      Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam aliquam sem et tortor consequat. Gravida arcu ac tortor dignissim. A pellentesque sit amet porttitor eget dolor morbi non. Nam aliquam sem et tortor consequat id porta.',
+      Description: 'Duracell Ultra DL123A CR123A 1470mAh 3V Lithium (LiMNO2) Battery. Bulk pack of 8.',
       Approved: true
     },
     {
@@ -27,8 +27,8 @@ exports.seed = function(knex, Promise) {
      Title: 'Chair',
      UserID: 100,
      Category: 'Home',
-     Price: 18,
-     Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam aliquam sem et tortor consequat. Gravida arcu ac tortor dignissim. A pellentesque sit amet porttitor eget dolor morbi non. Nam aliquam sem et tortor consequat id porta.',
+     Price: 50,
+     Description: 'Good Condition. There is minor wear and tear on the left leg of chair. Have been told it is extrememly comfortable.',
      Approved: true
    },
    {
@@ -36,8 +36,8 @@ exports.seed = function(knex, Promise) {
      Title: 'TV',
      UserID: 400,
      Category: 'Electronics',
-     Price: 150,
-     Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam aliquam sem et tortor consequat. Gravida arcu ac tortor dignissim. A pellentesque sit amet porttitor eget dolor morbi non. Nam aliquam sem et tortor consequat id porta.',
+     Price: 500,
+     Description: 'Perfect condition. Remote not included.',
      Approved: true
    },
    {
@@ -45,8 +45,8 @@ exports.seed = function(knex, Promise) {
      Title: 'Physics book',
      UserID: 600,
      Category: 'Books',
-     Price: 20,
-     Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam aliquam sem et tortor consequat. Gravida arcu ac tortor dignissim. A pellentesque sit amet porttitor eget dolor morbi non. Nam aliquam sem et tortor consequat id porta.',
+     Price: 100,
+     Description: 'Necessary for Physics 220 and Physics 230. Special edition from the bookstore that does not include access code.',
      Approved: true
    },
    {
@@ -54,8 +54,8 @@ exports.seed = function(knex, Promise) {
      Title: 'Calculus book',
      UserID: 700,
      Category: 'Books',
-     Price: 10,
-     Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam aliquam sem et tortor consequat. Gravida arcu ac tortor dignissim. A pellentesque sit amet porttitor eget dolor morbi non. Nam aliquam sem et tortor consequat id porta.',
+     Price: 80,
+     Description: 'Never opened. Still in the plastic film. I bought it for Math 227 and my teacher ended up switching the book last minute.',
      Approved: true
    },
    {
@@ -63,17 +63,17 @@ exports.seed = function(knex, Promise) {
     Title: 'Table',
     UserID: 200,
     Category: 'Home',
-    Price: 25,
-    Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam aliquam sem et tortor consequat. Gravida arcu ac tortor dignissim. A pellentesque sit amet porttitor eget dolor morbi non. Nam aliquam sem et tortor consequat id porta.',
+    Price: 150,
+    Description: 'Brand new IKEA Table and has not been assembled.',
     Approved: true
   },
   {
    ID: 9000,
    Title: 'Laptop',
    UserID: 300,
-   Price: 125,
+   Price: 800,
    Category: 'Electronics',
-   Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam aliquam sem et tortor consequat. Gravida arcu ac tortor dignissim. A pellentesque sit amet porttitor eget dolor morbi non. Nam aliquam sem et tortor consequat id porta.',
+   Description: 'Surface Laptop 2. Ordered the wrong color on Black Friday. Never opened.',
    Approved: true
  }
  ]);
